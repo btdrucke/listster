666be4e2464944aa5b86f2aca03a2f5944793582
Benjamin T. Drucker <benjamin.drucker@nike.com>
2016-08-01 19:55:25 -0400

    Get user name and photo URI

--- app/src/main/java/com/whizbang/listster/MainActivity.java
+++ app/src/main/java/com/whizbang/listster/MainActivity.java
@@ -1,14 +1,21 @@
 package com.whizbang.listster;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser mUser;
+    private String mDisplayName;
+    private Uri mPhotoUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
             startActivity(new Intent(this, GoogleSignInActivity.class));
+        } else {
+            getUserData(user);
+            setTitle(mDisplayName);
+        }
+    }
+
+
+    private void getUserData(FirebaseUser user) {
+        mDisplayName = user.getDisplayName();
+        mPhotoUri = user.getPhotoUrl();
+        for (UserInfo profile : user.getProviderData()) {
+            if (mDisplayName == null) {
+                mDisplayName = profile.getDisplayName();
+            }
+            if (mPhotoUri == null) {
+                mPhotoUri = profile.getPhotoUrl();
+            }
         }
     }
 }
